fd2c5ab104ac6941c11ca54becd87572f68f8f0a
Mohammed Jalal <166988441+mohammedJalal99@users.noreply.github.com>
2026-04-26 13:46:08 +0300

    Create rich-editor-heroicons.php

---
+++ resources/lang/ckb/rich-editor-heroicons.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'action_label' => 'زیادکردنی Heroicon',
+    'heading' => 'دانانی Heroicon',
+    'heading_edit' => 'دەستکاریکردنی Heroicon',
+    'label' => 'ئایکۆن',
+    'placeholder' => 'ئایکۆنێک هەڵبژێرە',
+    'below_content' => 'ناوی کورتکراوەی (slug) ئەو :link-heroicon بنووسە کە دەتەوێت دایبنێیت.',
+    'alignment_label' => 'ڕیزبەندی',
+    'alignment_inline' => 'لە ناو دێڕدا (Inline)',
+    'alignment_left' => 'چەپ',
+    'alignment_right' => 'ڕاست',
+    'alignment_center' => 'ناوەڕاست',
+    'style_label' => 'شێواز',
+    'style_outline' => 'دەورگیراو (Outline)',
+    'style_solid' => 'پڕ (Solid)',
+    'style_mini' => 'بچووک (Mini)',
+    'size_label' => 'قەبارە',
+    'size_sm' => 'S (:pxpx)',
+    'size_md' => 'M (:pxpx)',
+    'size_lg' => 'L (:pxpx)',
+    'size_xl' => 'XL (:pxpx)',
+    'color_label' => 'ڕەنگ',
+    'aria_label_label' => 'ناونیشانی تایبەت بە Accessibility',
+    'aria_label_placeholder' => 'بە خواستی خۆتە - بۆ ئایکۆنی ڕازاندنەوە بەتاڵی بهێڵەرەوە',
+    'aria_label_helper' => 'پێناسەی ئایکۆنەکە دەکات بۆ خوێنەرەوەی شاشە (Screen Readers). ئەگەر ئایکۆنەکە تەنها بۆ ڕازاندنەوەیە، بەتاڵی بهێڵەرەوە.',
+];
